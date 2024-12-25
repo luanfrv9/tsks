8986d88fd90e71da5a4974d7676490425436cad2
luan ramos vicente <luanfrv9@gmail.com>
2024-12-24 23:09:16 -0300

    chore: update dependencies

--- web/jest.config.js
+++ web/jest.config.js
@@ -142,7 +142,7 @@ const config = {
   setupFiles: ['jest-localstorage-mock'],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['@testing-library/jest-dom'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
